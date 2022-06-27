4b1322ce00793b80f469164806ffb16eef04accc
Taron Grigoryan <1991taron@gmail.com>
2022-06-27 18:03:39 +0400

    Add project files.

---
+++ PersonService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadFromFile
+{
+
+    public class PersonService
+    {
+        private readonly string path = @"persons";
+        private readonly string Fnames;
+        private readonly string Lnames;
+        public PersonService()
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            path = Path.Combine(path, "persons.txt");
+            Fnames = Path.Combine(path, "FirstNames.txt");
+            Lnames = Path.Combine(path, "LastNames");
+
+        }
+        public void Create(Person person)
+        {
+            string[] personstr = new string[4];
+            personstr[0] = person.Id.ToString();
+            personstr[1] = person.FirstName;
+            personstr[2] = person.LastName;
+            personstr[3] = person.Age.ToString();
+            File.AppendAllLines(path, personstr);
+        }
+        public List<Person> ConvertToPerson(string[] persons)
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < persons.Length; i=i+4)
+            {
+                people.Add(new Person()
+                {
+                    Id = Guid.Parse(persons[i]),
+                    Age = Convert.ToInt16(persons[i+3]),
+                    LastName = persons[i+2],
+                    FirstName = persons[i+1]
+                });
+            }
+            return people;
+        }
+        public string[] Read()
+        {
+            string[] persons;
+            persons=File.ReadAllLines(path);
+            return persons;
+        }
+        public void Print(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"ID: {person.Id.ToString()}\n" +
+                    $"AGE: {person.Age}\n" +
+                    $"LAST NAME: {person.LastName}\n" +
+                    $"FIRST NAME: {person.FirstName}\n" +
+                    $"{new string('_', 30)}");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LoadFromFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonService ps = new PersonService();
+            List<Person> people = new List<Person>();
+            ps.Create(new Person { Age = new Random().Next(1,99), LastName = "Poghosyan", FirstName = "Poghos" });
+            ps.Create(new Person { Age = new Random().Next(1, 99), LastName = "Petrosyan", FirstName = "Petros" });
+            ps.Create(new Person { Age = new Random().Next(1, 99), LastName = "Martirosyan", FirstName = "Martisros" });
+            ps.Create(new Person { Age = new Random().Next(1, 99), LastName = "Khachikyan", FirstName = "Khachik" });
+          var peoples= ps.ConvertToPerson(ps.Read());
+            ps.Print(peoples);
+        }
+    }
+
+
+
+}
