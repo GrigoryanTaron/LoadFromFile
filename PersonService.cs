3b71d4a3ee218aa46974e1d1c27495271ab0a13d
Taron Grigoryan <1991taron@gmail.com>
2022-06-29 13:08:19 +0400

    Changed to Filestrem

--- PersonService.cs
+++ PersonService.cs
@@ -15,42 +15,59 @@ namespace LoadFromFile
         private readonly string Lnames = @"persons";
         public PersonService()
         {
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
             path = Path.Combine(path, "persons.txt");
             Fnames = Path.Combine(Fnames, "FirstNames.txt");
             Lnames = Path.Combine(Lnames, "LastNames.txt");
-
         }
         public void Create(Person person)
         {
             string[] personstr = new string[4];
-            personstr[0] = person.Id.ToString();
-            personstr[1] = person.FirstName;
-            personstr[2] = person.LastName;
-            personstr[3] = person.Age.ToString();
-            File.AppendAllLines(path, personstr);
+            personstr[0] = person.Id.ToString() + "\n";
+            personstr[1] = person.FirstName + "\n";
+            personstr[2] = person.LastName + "\n";
+            personstr[3] = person.Age.ToString() + "\n";
+
+            using (FileStream fstream = new FileStream(path, FileMode.Append))
+            {
+                for (int i = 0; i < personstr.Length; i++)
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(personstr[i]);
+                    fstream.Write(buffer, 0, buffer.Length);
+                }
+            }
         }
         public List<Person> ConvertToPerson(string[] persons)
         {
             List<Person> people = new List<Person>();
             for (int i = 0; i < persons.Length; i = i + 4)
             {
-                people.Add(new Person()
+                try
                 {
-                    Id = Guid.Parse(persons[i]),
-                    Age = Convert.ToInt16(persons[i + 3]),
-                    LastName = persons[i + 2],
-                    FirstName = persons[i + 1]
-                });
+                    people.Add(new Person()
+                    {
+                        Id = Guid.Parse(persons[i]),
+                        Age = Convert.ToInt16(persons[i + 3]),
+                        LastName = persons[i + 2],
+                        FirstName = persons[i + 1]
+                    });
+                }
+                catch
+                {
+                }
             }
             return people;
         }
         public string[] Read()
         {
             string[] persons;
-            persons = File.ReadAllLines(path);
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[fstream.Length];
+                fstream.Read(buffer, 0, buffer.Length);
+                string textFromFile = Encoding.Default.GetString(buffer);
+                fstream.Seek(-1, SeekOrigin.End);
+                persons = textFromFile.Split("\n");
+            }
             return persons;
         }
         public string[] NamesGen()
